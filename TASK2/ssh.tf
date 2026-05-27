892875806b68cd7b5a34a0286543aa57c7239687
Atharv1906 <anujsingh1962003@gmail.com>
2026-05-27 12:32:43 +0530

    final commit

--- TASK2/ssh.tf
+++ TASK2/ssh.tf
@@ -15,9 +15,14 @@ locals {
   }
 }
 
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_key_pair" "this" {
   key_name   = local.names.key_pair
-  public_key = var.ssh_key
+  public_key = tls_private_key.this.public_key_openssh
 
   tags = local.common_tags
 }
--- TASK2/variables.tf
+++ TASK2/variables.tf
@@ -7,9 +7,3 @@ variable "instance_type" {
   description = "The type of EC2 instance to use"
   type        = string
 }
-
-variable "ssh_key" {
-  description = "Provides custom public SSH key."
-  type        = string
-  sensitive   = true
-}
